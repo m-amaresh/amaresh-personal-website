de506eaa4ed77cd58242ef8888d8062e05fbee7a
Amaresh Mohapatra <m.amaresh@hotmail.com>
2023-04-08 04:08:19 +0530

    fix
    Signed-off-by: Amaresh Mohapatra <m.amaresh@hotmail.com>

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as  Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Resume from './Resume';
 
